514da4d51a257ab314adeec550e0fc09217c2bfe
jesaulopez <111526976+jesaulopez@users.noreply.github.com>
2022-11-01 17:23:57 -0500

    Añado archivo Reto 1

---
+++ src/test/java/FechaDeNacimiento.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+
+public class FechaDeNacimiento {
+
+    String date = "21-02-1990";
+    @Test
+    public void birthdayDate(){
+        System.out.printf("Mi Fecha de Nacimiento es: "+ date);
+    }
+}
